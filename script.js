d5c939e3cc1fa854a16c18ab08171ec5a5b6b47d
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2023-07-01 11:43:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+/********coki */
+if (localStorage.getItem("agreementHidden")) {
+  document.querySelector(".agreement-container").style.display = "none";
+}
+document.querySelector(".agreement-button").addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector(".agreement-container").style.display = "none";
+  localStorage.setItem("agreementHidden", true);
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var navIcons = document.querySelectorAll('.mobile');
+  
+  function toggleClass() {
+    this.classList.toggle('show');
+  }
+  
+  for (var i = 0; i < navIcons.length; i++) {
+    navIcons[i].addEventListener('click', toggleClass);
+  }
+});
+
+
+
+let headerNav = document.querySelector('.header-nav');
+
+let previousScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+function handleScroll() {
+  var currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  
+  if (currentScrollPosition > previousScrollPosition) {
+    headerNav.classList.add('scrolled-down');
+  } else {
+    headerNav.classList.remove('scrolled-down');
+  }
+  
+  previousScrollPosition = currentScrollPosition;
+}
+
+window.addEventListener('scroll', handleScroll);
